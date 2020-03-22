1d696958981deefa680849c7c1f27eb5105dcf3d
elcordova <erick.lenin.03@gmail.com>
2020-03-22 15:37:31 -0500

    first end template

---
+++ data.js
@@ -0,0 +1,48 @@
+const main = {
+  name: 'Erick Córdova',
+  mail: 'erick.lenin.03@gmail.com',
+  img: 'https://s.gravatar.com/avatar/26b27e7762b8bb9cd039ea765e6ae6e3?s=400',
+  role: ['Software Solutions Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://elcordova.github.io/'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/elcordova'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/erick-cordova-29a813125/'
+    },
+    {
+      name: 'StackOverflow',
+      iconName: 'logo-stackoverflow',
+      link: 'https://stackoverflow.com/users/7420509/elcordova'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/erickLeninC'
+    },
+    {
+      name: 'npm',
+      iconName: 'logo-npm',
+      link: 'https://www.npmjs.com/~elcordova'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'erick.lenin.03@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
